5f2dd69d46cd65610e66b3e9f32693a6aaab8d61
fherreraunahur <141511047+fherreraunahur@users.noreply.github.com>
2023-08-22 23:04:16 +0000

    actualizacion

--- Hola.py
+++ Hola.py
@@ -1,2 +1,4 @@
-print("Hola bienvenidos a Pensamiento Computacional")
-print("Escribir tu nombre")
+num1=float(input('Ingrese num1'))
+num2=int(input('Ingrese num2'))
+suma=num1+num2
+print(f"La suma de {num1} + {num2} es {suma}")
